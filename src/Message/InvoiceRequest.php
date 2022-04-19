f42e94a0001d9d669602c48bd0fa8224d9bc9260
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2022-04-19 23:23:14 +0300

    Update InvoiceRequest.php

--- src/Message/InvoiceRequest.php
+++ src/Message/InvoiceRequest.php
@@ -7,8 +7,8 @@ use Omnipay\Common\Exception\InvalidRequestException;
 class InvoiceRequest extends AbstractRequest
 {
     protected $method     = 'POST';
-    public $productionUri = "https://api.aws.bitbanker.ru/latest/api/v1/invoices";
-    public $testUri       = "https://api.dev.aws.bitbanker.ru/latest/api/v1/invoices";
+    public $productionUri = "https://api.aws.bitbanker.org/latest/api/v1/invoices";
+    public $testUri       = "https://api.dev.aws.bitbanker.org/latest/api/v1/invoices";
 
     /**
      * @throws InvalidRequestException
