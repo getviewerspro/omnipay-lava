b9d99a5ef0b6b697bef74aeb9d2eb11011148a21
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2023-03-22 15:21:38 +0300

    Update AbstractRequest.php

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -71,6 +71,9 @@ abstract class AbstractRequest extends Request
     public function send()
     {
         $data = $this->getData();
+        
+        info($data);
+        
         $response = $this->getClient($data);
         $result = json_decode($response->getBody()->getContents(),1);
         return $this->sendData($result);
