12a9a5d3f0d26f391cd18cbbccdd6648435b64f0
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2023-03-24 00:17:50 +0300

    Create CompletePurchaseRequest.php

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Lava driver for Omnipay PHP payment library
+ *
+ * @link      https://github.com/getviewerspro/omnipay-lava
+ * @package   omnipay-lava
+ * @license   MIT
+ * @copyright Copyright (c) 2023, getViewersPRO (https://getviewers.pro/)
+ */
+
+namespace Omnipay\Lava\Message;
+
+/**
+ * Lava Complete Purchase Request.
+ */
+class CompletePurchaseRequest extends AbstractRequest
+{
+    /**
+     * Get the data for this request.
+     * @return array request data
+     */
+    public function getData()
+    {
+        return $this->httpRequest->request->all();
+    }
+
+    /**
+     * Send the request with specified data.
+     * @param mixed $data The data to send
+     * @return CompletePurchaseResponse
+     */
+    public function sendData($data)
+    {
+        return $this->response = new CompletePurchaseResponse($this, $data);
+    }
+}
