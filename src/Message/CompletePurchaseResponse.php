5568a0812800be5387de6a95904ba3107b9f5880
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2023-03-24 00:16:02 +0300

    Update and rename CompletePurchaseRequest.php to CompletePurchaseResponse.php

---
+++ src/Message/CompletePurchaseResponse.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Lava driver for Omnipay PHP payment library
+ *
+ * @link      https://github.com/getviewerspro/omnipay-lava
+ * @package   omnipay-lava
+ * @license   MIT
+ * @copyright Copyright (c) 2023, getViewersPRO (https://getviewers.pro/)
+ */
+
+namespace Omnipay\Lava\Message;
+
+use Omnipay\Common\Exception\InvalidResponseException;
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
+
+/**
+ * Digiseller Complete Purchase Response.
+ */
+class CompletePurchaseResponse extends AbstractResponse
+{
+    /**
+     * @var CompletePurchaseRequest|RequestInterface
+     */
+    protected $request;
+    protected $customFields;
+
+    public function __construct(RequestInterface $request, $data)
+    {
+        $this->request = $request;
+        $this->data    = $this->request->getApiData(); 
+
+        if ($this->getSign() !== $this->calculateSignature()) {
+            throw new InvalidResponseException('Invalid hash');
+        }
+    }
+
+    public function isSuccessful()
+    {
+        return $this->data['status'] == 0 ? true : false;
+    }
+
+    public function getTransactionId()
+    {
+        return $this->data['order_id'];
+    }
+
+    public function getAmount()
+    {
+        return (string)$this->data['amount'];
+    }  
+
+    public function getCurrency()
+    {
+        return 'RUB';
+    }    
+    
+    public function getMoney()
+    {
+        return (string)$this->data['credited']();
+    }
+
+    public function getTransactionReference()
+    {
+        return $this->data['invoice_id'];
+    }
+
+    public function getSign()
+    {
+        return $this->request()->header('Authorization');
+    }
+    
+    public function calculateSignature()
+    {
+        $signStr = json_encode($this->data['custom_fields'], JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        
+        return hash_hmac(
+                'sha256',
+                $signStr,
+                $this->request->getSignAdd()
+            );
+    }
+}
